892c3b81037ece0e1753ab058e3cfda93f985693
Protesilaos Stavrou <info@protesilaos.com>
2025-07-22 09:06:09 +0300

    Update to Cursory version 1.2.0

--- cursory.el
+++ cursory.el
@@ -5,7 +5,7 @@
 ;; Author: Protesilaos Stavrou <info@protesilaos.com>
 ;; Maintainer: Protesilaos Stavrou <info@protesilaos.com>
 ;; URL: https://github.com/protesilaos/cursory
-;; Version: 1.1.0
+;; Version: 1.2.0
 ;; Package-Requires: ((emacs "29.1"))
 ;; Keywords: convenience, cursor
 
